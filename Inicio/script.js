fe92e58c802966b6295c72678190ff4946a0d5fb
Luz3ReI <luz441rovira@gmail.com>
2024-04-08 20:03:48 -0600

    Carrusel Final

---
+++ Inicio/script.js
@@ -0,0 +1,22 @@
+
+//Inicio Carrusel
+
+'use strict'
+
+const grande    = document.querySelector('.grande')
+const punto     = document.querySelectorAll('.punto')
+
+punto.forEach( ( cadaPunto , i )=> {
+    punto[i].addEventListener('click',()=>{
+        let posicion  = i
+        let operacion = posicion * -33
+        grande.style.transform = `translateX(${ operacion }%)`
+        punto.forEach( ( cadaPunto , i )=>{
+            punto[i].classList.remove('activo')
+        })
+        punto[i].classList.add('activo')
+
+    })
+})
+
+//Fin Carrusel
